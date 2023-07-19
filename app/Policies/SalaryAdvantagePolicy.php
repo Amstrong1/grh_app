7a78c0f1ea217b111a72a1a7d546f9797d5564c7
Amstrong1 <lewisamstrongh@gmail.com>
2023-07-19 20:51:55 +0100

    autoload correction

--- app/Policies/SalaryAdvantagePolicy.php
+++ app/Policies/SalaryAdvantagePolicy.php
@@ -6,7 +6,7 @@ use App\Models\SalaryAdvantage;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class SalaryAdvantagesPolicy
+class SalaryAdvantagePolicy
 {
     /**
      * Determine whether the user can view any models.
