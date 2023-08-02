fbba7b93ca7bed1f76d8140401b7b6590ea7e7d7
Amstrong1 <lewisamstrongh@gmail.com>
2023-08-02 09:48:19 +0100

    index leave sold fix

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -323,7 +323,7 @@
                                 </div>
                                 <div>
                                     <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-                                        Solde Congés : {{ $sold }}
+                                        Solde Congés : {{ $leaveSold }}
                                     </p>
                                     <p class="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-200">
                                     </p>
