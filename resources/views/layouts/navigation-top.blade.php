5bc361a642175cd8e540ea35b92d16d09e053155
Amstrong1 <lewisamstrongh@gmail.com>
2023-11-21 17:39:28 +0100

    logo change

--- resources/views/layouts/navigation-top.blade.php
+++ resources/views/layouts/navigation-top.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <img class="h-14" src="{{ url('storage/' . Auth::user()->structure->logo) }}"
+                        <img class="h-14" src="{{ url('assets/img/vinci.jpg') }}"
                             alt="{{ Auth::user()->structure->name }}" loading="lazy">
                     </a>
                 </div>
